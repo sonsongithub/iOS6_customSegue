477df06b6efd23668db4bab39c87542aa597c0d3
sonsongithub <yoshida.yuichi@gmail.com>
2012-11-24 10:07:24 +0900

    Added the animation code to MyDismisssingSegue class.

--- iOS6_customSegue/MyDismissingSegue.m
+++ iOS6_customSegue/MyDismissingSegue.m
@@ -19,35 +19,40 @@
 
 - (void)perform {
 	NSLog(@"MyDismissingSegue - perform");
-	UIView *sourceView = ((UIViewController*)self.sourceViewController).view;
-	UIView *destinationView = ((UIViewController*)self.destinationViewController).view;
+	
+	void (^completionMethod)()
+	= ^() {
+		UIView *sourceView = ((UIViewController*)self.sourceViewController).view;
+		UIView *destinationView = ((UIViewController*)self.destinationViewController).view;
+		
+		[destinationView addSubview:sourceView];
+		
+		CAKeyframeAnimation *sizeAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+		sizeAnimation.values = @[@(1), @(0)];
+		sizeAnimation.keyTimes = @[@(0), @(1)];
+		
+		CAKeyframeAnimation *rotateAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation"];
+		rotateAnimation.values = @[@(0), @(M_PI*2)];
+		rotateAnimation.keyTimes = @[@(0), @(1)];
+		
+		
+		// make group
+		CAAnimationGroup *group = [CAAnimationGroup animation];
+		group.animations = [NSArray arrayWithObjects:sizeAnimation, rotateAnimation, nil];
+		group.duration = 0.4;
+		group.removedOnCompletion = NO;
+		group.fillMode = kCAFillModeForwards;
+		group.delegate = self;
+		
+		// set context
+		[group setValue:sourceView forKey:@"context"];
+		
+		// commit animation
+		[sourceView.layer addAnimation:group forKey:@"hoge"];
+	};
+	
 	[self.sourceViewController dismissViewControllerAnimated:NO
-												  completion:^() {
-													  [destinationView addSubview:sourceView];
-													  
-													  CAKeyframeAnimation *sizeAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
-													  sizeAnimation.values = @[@(1), @(0)];
-													  sizeAnimation.keyTimes = @[@(0), @(1)];
-													  
-													  CAKeyframeAnimation *rotateAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation"];
-													  rotateAnimation.values = @[@(0), @(M_PI*2)];
-													  rotateAnimation.keyTimes = @[@(0), @(1)];
-													  
-													  
-													  // make group
-													  CAAnimationGroup *group = [CAAnimationGroup animation];
-													  group.animations = [NSArray arrayWithObjects:sizeAnimation, rotateAnimation, nil];
-													  group.duration = 0.4;
-													  group.removedOnCompletion = NO;
-													  group.fillMode = kCAFillModeForwards;
-													  group.delegate = self;
-													  
-													  // set context
-													  [group setValue:sourceView forKey:@"context"];
-													  
-													  // commit animation
-													  [sourceView.layer addAnimation:group forKey:@"hoge"];
-												  }];
+												  completion:completionMethod];
 }
 
 @end
